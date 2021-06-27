8e768f11d885f8f83da72373a0019cc600870533
Max Klein <max.klein@grandcentrix.net>
2021-06-27 12:01:04 +0200

    added keyboard listener

---
+++ js/logic/keys.js
@@ -0,0 +1,37 @@
+var keys = {
+    accelerate: false,
+    left: false,
+    up: false,
+    right: false,
+    down: false,
+
+    init: function() {
+        document.addEventListener('keydown', function(event) {
+            keys.handler(event, true)
+        })
+        document.addEventListener('keyup', function(event) {
+            keys.handler(event, false)
+        })
+    },
+    handler: function(event, status) {
+        switch (event.key) {
+            case "Ctrg":
+                keys.accelerate = status
+                break;
+            case "ArrowDown":
+                keys.down = status
+                break;
+            case "ArrowUp":
+                keys.up = status
+                break;
+            case "ArrowLeft":
+                keys.left = status
+                break;
+            case "ArrowRight":
+                keys.right = status
+                break;
+            default:
+                return; // Quit when this doesn't handle the key event.
+        }
+    }
+}
--- js/logic/levelController.js
+++ js/logic/levelController.js
@@ -5,9 +5,10 @@ function levelController() {
             new Entity({ x: 232, y: 112 }, { height: 23, width: 56 }),
             new MovingEntity({ x: 50, y: 500 }, { height: 16, width: 500 }, 250, 2.4, MovingEntity.VERTICAL),
         ])
-    document.getElementById("title").innerHTML = level.name
 
-    console.log(JSON.stringify(level))
+    keys.init()
+
+    document.getElementById("title").innerHTML = level.name
 
     setInterval(
         () => {
@@ -30,48 +31,3 @@ function buildLvl(level) {
         game.appendChild(element.createHtml())
     });
 }
-
-// function keyBoardListner(params) {
-//     document.addEventListener('keydown', function(event) {
-//         switch (event.key) {
-//             case "ArrowDown":
-//                 console.log("down")
-//                 if (isArrowDown == false) {
-//                     isArrowDown = true;
-//                     level.Entitys[0].onTick(1)
-//                 }
-//                 break;
-//             case "ArrowUp":
-//                 // code for "up arrow" key press.
-//                 break;
-//             case "ArrowLeft":
-//                 // code for "left arrow" key press.
-//                 break;
-//             case "ArrowRight":
-//                 // code for "right arrow" key press.
-//                 break;
-//             default:
-//                 return; // Quit when this doesn't handle the key event.
-//         }
-//     });
-
-//     document.addEventListener('keyup', function(event) {
-//         switch (event.key) {
-//             case "ArrowDown":
-//                 console.log("up")
-//                 this.isArrowDown = true;
-//                 break;
-//             case "ArrowUp":
-//                 // code for "up arrow" key press.
-//                 break;
-//             case "ArrowLeft":
-//                 // code for "left arrow" key press.
-//                 break;
-//             case "ArrowRight":
-//                 // code for "right arrow" key press.
-//                 break;
-//             default:
-//                 return; // Quit when this doesn't handle the key event.
-//         }
-//     });
-// }
