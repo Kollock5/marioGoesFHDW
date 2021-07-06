5d438db5761974b3fbdb91dce57f63bed0222f0d
Max Klein <max.klein@grandcentrix.net>
2021-07-06 15:26:41 +0200

    fixed spelling

--- js/util/collisionDetection.js
+++ js/util/collisionDetection.js
@@ -1,17 +1,9 @@
-/* 
-Dont ask why... it just works...
-
-i have to touch and redo mutch of this code later at some point again but my brain hurts way to mutch...
-
-x and y axis and collidingOnX and Y do the same thing but i cant find a more clever way to put them togther
-for now... also the Axis part is way to mutch spaghetti but i cant find a smarter way at the moment...
-*/
-export var collisonDetection = {
+export var collisionDetection = {
     checkXAxisColliding: function(level, entity) {
         var collider = null
         var movement = null;
         level.entities.forEach(them => {
-            var newMovement = collisonDetection.collidingOnX(entity, them);
+            var newMovement = collisionDetection.collidingOnX(entity, them);
             if (newMovement != null) {
                 if (movement == null) {
                     movement = newMovement;
@@ -22,8 +14,8 @@ export var collisonDetection = {
                 }
             }
         });
-        var collisonPos = ((movement != null) ? (entity.pos.x + movement) : null)
-        return { collider: collider, collisonPos: collisonPos }
+        var collisionPos = ((movement != null) ? (entity.pos.x + movement) : null)
+        return { collider: collider, collisionPos: collisionPos }
     },
 
     collidingOnX: function(us, them) {
@@ -32,11 +24,11 @@ export var collisonDetection = {
             (us.pos.y + us.size.width) > them.pos.y &&
             us != them
         ) {
-            //Check for collison in negativ direction
+            //Check for collision in negative direction
             if (us.pos.x >= (them.pos.x + them.size.height) && (us.pos.x + us.vel.x) <= (them.pos.x + them.size.height)) {
                 return (them.pos.x + them.size.height - us.pos.x)
             }
-            //Check for collison in positiv direction
+            //Check for collision in positive direction
             if ((us.pos.x + us.size.height) <= them.pos.x && ((us.pos.x + us.size.height) + us.vel.x) >= them.pos.x) {
                 return (them.pos.x - us.size.height - us.pos.x)
             }
@@ -48,7 +40,7 @@ export var collisonDetection = {
         var collider = null
         var movement = null;
         level.entities.forEach(them => {
-            var newMovement = collisonDetection.collidingOnY(entity, them);
+            var newMovement = collisionDetection.collidingOnY(entity, them);
             if (newMovement != null) {
                 if (movement == null) {
                     movement = newMovement;
@@ -59,8 +51,8 @@ export var collisonDetection = {
                 }
             }
         });
-        var collisonPos = ((movement != null) ? (entity.pos.y + movement) : null)
-        return { collider: collider, collisonPos: collisonPos }
+        var collisionPos = ((movement != null) ? (entity.pos.y + movement) : null)
+        return { collider: collider, collisionPos: collisionPos }
     },
 
     collidingOnY: function(us, them) {
@@ -69,11 +61,11 @@ export var collisonDetection = {
             (us.pos.x + us.size.height) > them.pos.x &&
             us != them
         ) {
-            //Check for collison in negativ direction
+            //Check for collision in negative direction
             if (us.pos.y >= (them.pos.y + them.size.width) && (us.pos.y + us.vel.y) <= (them.pos.y + them.size.width)) {
                 return (them.pos.y + them.size.width - us.pos.y)
             }
-            //Check for collison in positiv direction
+            //Check for collision in positive direction
             if ((us.pos.y + us.size.width) <= them.pos.y && ((us.pos.y + us.size.width) + us.vel.y) >= them.pos.y) {
                 return (them.pos.y - us.size.width - us.pos.y)
             }
