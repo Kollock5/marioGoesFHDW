cbc46a93b42d746638331971ea16441b44b5835f
Max Klein <max.klein@grandcentrix.net>
2021-09-01 15:59:03 +0200

    fixed naming

--- js/entities/DirtBlock.js
+++ js/entities/DirtBlock.js
@@ -1,4 +1,4 @@
 import { Entity } from "../Entity.js"
 import { Vector } from "../util/Vector.js"
 
-export function DirtBlock(pos) { return new Entity(pos, new Vector(32, 32), "../res/dirt.png") }
+export function DirtBlock(pos) { return new Entity(pos, new Vector(32, 32), "../res/dirt_block.png") }
