1e891b08a7a8d49210f54f160c3445d475bb2088
Max Klein <max.klein@grandcentrix.net>
2021-09-02 13:53:55 +0200

    res fixes

--- js/entities/FlowerRed.js
+++ js/entities/FlowerRed.js
@@ -2,4 +2,4 @@ import { Entity } from "../Entity.js"
 import { Vector } from "../util/Vector.js"
 import { BackgroundItem } from "../properties/BackgroundItem.js"
 
-export function FlowerRed(pos = new Vector(0, 0)) { return new Entity(pos, new Vector(32, 32), "../res/flower.png", [new BackgroundItem(1)]) }
+export function FlowerRed(pos = new Vector(0, 0)) { return new Entity(pos, new Vector(32, 32), "../res/flower.png", [new BackgroundItem(2)]) }
